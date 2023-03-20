f1e3e2a9e4cb1f9eae007b4aa5c4aa83c0d039f5
Mohammad Sakib Chowdhury <60073465+sakib071@users.noreply.github.com>
2023-03-21 01:53:48 +0600

    Add files via upload

---
+++ C193055_HHJN_Lab-02.cc
@@ -0,0 +1,33 @@
+/*
+ * C193055_HHJN_Lab-02.cc
+ *
+ *  Created on: Jan 11, 2023
+ *      Author: mdsak
+ */
+
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class C193055_HHJN : public cSimpleModule
+{
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(C193055_HHJN);
+
+void C193055_HHJN::initialize()
+{
+    if (strcmp("tower1", getName()) == 0) {
+        cMessage *msg = new cMessage("C193055_HHJN_Message");
+        send(msg, "out");
+    }
+}
+
+void C193055_HHJN::handleMessage(cMessage *msg)
+{
+    send(msg, "out");
+}
